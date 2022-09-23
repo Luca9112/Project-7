351e758409bf9cf2f8692e8d8c8b8bac34449289
luca6969 <Lucacoppens19.0@gmail.com>
2022-09-23 11:49:57 +0200

    hoi
    hoi

---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html> 
+<html lang="en" dir="ltr"> 
+    <body> 
+        <nav> 
+            <div> 
+                <ul> 
+                    <li> <a href="index.php">home</a></li>
+                    <li> <a href="reserveren.php">reserveren</a></li> 
+                    <li> <a href="menu.php">Menu's bekijken</a></li> 
+                    <li> <a href="signup.php">Account aanmaken </a></li> 
+                    <li> <a href="login.php">Log in </a></li>  
+                </ul> 
+            </div>
