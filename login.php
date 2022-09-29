4aae3bfb68302d11ed318566bbe25c4f65c515b8
luca6969 <Lucacoppens19.0@gmail.com>
2022-09-29 11:19:38 +0200

    .

--- login.php
+++ login.php
@@ -3,11 +3,9 @@
 ?> 
 
     <section class="signup-form"> 
-        <h2> Sign up</h2> 
-        <form action="signup.inc.php" method="post"> <br>
-         <input type="text" name="name" placeholder="Voornaam & achternaam..."> <br> 
+        <h2> Log in</h2> 
+        <form action="login.inc.php" method="post">  
          <input type="text" name="Email" placeholder="E-mail..."><br>
-         <input type="text" name="telefoon" placeholder="Telefoon nummer..."> <br> 
          <input type="text" name="wachtwoord" placeholder="Wachtwoord..."> <br> 
         <button type="submit" name="submit">Signup </button>         
         </form> 
