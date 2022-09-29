6bfa5f10993ce8f66dd4263014270032f67872c8
luca6969 <Lucacoppens19.0@gmail.com>
2022-09-29 11:04:29 +0200

    .

--- signup.php
+++ signup.php
@@ -1,9 +1,9 @@
 <?php 
-    include_once'header.php'; 
+    include_once 'header.php'; 
 ?> 
 
     <section class="signup-form"> 
-        <h2>Sign up</h2> 
+        <h2> Sign up</h2> 
         <form action="signup.inc.php" method="post">
          <input type="text" name="name" placeholder="Voornaam & achternaam...">
          <input type="text" name="Email" placeholder="E-mail...">
---
+++ test.php
@@ -0,0 +1,4 @@
+<!doctype html>
+<?php 
+    include_once'header.php'; 
+?> 
