54dc74dd65b5a9213d0ce079b40c7a744fca6581
jonleijgraaf <105272112+123jon12@users.noreply.github.com>
2022-11-07 09:37:03 +0100

    verbeterde alles

--- header.php
+++ header.php
@@ -1,27 +1,48 @@
+<?php
+    session_start(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Links -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">  
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
+    <div class="container px-5">
+        <a class="navbar-brand" href="index.php">Bon Temps</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+
 <?php 
-    	session_start(); 
-?> 
+    if (isset($_SESSION["useruid"])){
+                echo "<li class='nav-item'><a class='nav-link' href='reserveren-overzicht.php'>zak </a></li>"; 
+                echo "<li class='nav-item'><a class='nav-link' href='profile.php'>profiel</a></li>" ;
+                echo "<li class='nav-item'><a class='nav-link' href='../includes/logout.inc.php'>Log uit</a></li>";  
+} 
+    else{ 
+        echo "<li class='nav-item'><a class='nav-link' href='signup.php'>account aanmaken </a></li>";
+        echo "<li class='nav-item'><a class='nav-link' href='login.php'>Log in</a></li>"; 
 
-<!doctype html> 
-<html lang="en" dir="ltr"> 
-    <body> 
-        <nav> 
-            <div> 
-                <ul> 
-                    <li> <a href="index.php">home</a></li>
-                    <li> <a href="reserveren-form.php">reserveren</a></li> 
-                    <li> <a href="reserveren-overzicht.php">overzicht reserveren</a></li> 
-                    <?php
-                      if (isset($_SESSION["useruid"])){ 
-                        echo "<li> <a href='profile.php'>profiel</a></li>"; 
-                        echo "<li> <a href='includes/logout.inc.php'>Log uit</a></li> "; 
-                      }  
-                      else { 
-                        echo "<li> <a href='signup.php'>Account aanmaken </a></li>";  
-                        echo "<li> <a href='login.php'>Log in </a></li>"; 
 
-                      }
-                    ?>
-                </ul> 
-            </div>
-        </nav>
+}
+
+
+        ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
